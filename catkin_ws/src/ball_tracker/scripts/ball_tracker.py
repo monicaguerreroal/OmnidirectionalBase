b3b65e85ce9a4b7042ca1312a8facf3dfb9cf93f
Hèctor Robles <robletes062901@gmail.com>
2022-06-09 13:30:50 -0500

    ball tracker corrected

--- catkin_ws/src/ball_tracker/scripts/ball_tracker.py
+++ catkin_ws/src/ball_tracker/scripts/ball_tracker.py
@@ -4,23 +4,35 @@ from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Twist
 
 def callback_ball_position(msg):
-    global pub_cmd_vel
+    global pub_cmd_vel,new_data
     print(msg.data)
-    K = 5.0/320.0
+    K = 2.0/320.0
     e = msg.data[0]-320
     cmd_vel = Twist()
     cmd_vel.angular.z = -K * e
     pub_cmd_vel.publish (cmd_vel)
+    new_data = True
+    
 
 def main ():
-    global pub_cmd_vel
+    global pub_cmd_vel, new_data
     print("initializing ball tracker")
     rospy.init_node("ball_tracker")
     pub_cmd_vel  = rospy.Publisher("/cmd_vel",Twist, queue_size=10)
     rospy.Subscriber("/ball_position",Float32MultiArray,callback_ball_position)
-    
-    
-    rospy.spin()
+    new_data = False
+    loop = rospy.Rate(50)
+    counter = 0
+    while not rospy.is_shutdown():
+        if not new_data:
+            counter+=1
+            if counter>10:
+                counter = 0
+                pub_cmd_vel.publish(Twist())
+        else:
+            counter = 0
+            new_data = False
+        loop.sleep() 
 
 if __name__== "__main__":
     main()
